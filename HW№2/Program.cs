cd5ef2141c5a7f44aabcc4b9d8b87ef5584b628b
Ivan <ivanmatveev1001@gmail.com>
2022-08-21 11:16:42 +0300

    Задание №2

---
+++ HW№2/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr [i] = new Random().Next (0,10);
+    }
+}
+
+void PrintArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{arr [i]} ");
+    }
+}
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 == 1)
+    {   
+        count = count + array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Сумма: ");
+Console.WriteLine(count);
