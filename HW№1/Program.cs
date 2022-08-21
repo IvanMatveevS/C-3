10d4f2bccabadbdb4a570399a5ac35f697b3ec66
Ivan <ivanmatveev1001@gmail.com>
2022-08-21 11:08:44 +0300

    Задание №1

---
+++ HW№1/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr [i] = new Random().Next (101,1000);
+    }
+}
+
+void PrintArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{arr [i]} ");
+    }
+}
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {   
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных:");
+Console.WriteLine(count);
