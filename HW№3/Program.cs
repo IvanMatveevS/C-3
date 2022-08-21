fdf27a2259e11465daaf1f38c884bbca4e5ec738
Ivan <ivanmatveev1001@gmail.com>
2022-08-21 11:38:13 +0300

    Задание №3

---
+++ HW№3/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr [i] = new Random().Next (0,1000);
+    }
+}
+
+void PrintArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{arr [i]} ");
+    }
+}
+int [] array = new int [8];
+FillArray(array);
+PrintArray(array);
+int count = 0;
+int P = 0;
+int Q = array[1];
+for (int i = 0; i < array.Length; i++)
+{
+    if (P < array [i])
+    {   
+        P = array [i];
+    }
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (Q > array [i])
+    {   
+        Q = array [i];
+    }
+}
+count = P - Q;
+
+ Console.WriteLine();
+ Console.WriteLine(P);
+ Console.WriteLine(Q);
+  Console.WriteLine("Разница: ");
+ Console.WriteLine(count);
