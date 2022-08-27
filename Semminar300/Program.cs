5815a8de0681ba1b08776a064afa8142a051c071
Ivan <ivanmatveev1001@gmail.com>
2022-08-27 19:20:02 +0300

    Задание двумерного массива

---
+++ Semminar300/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите высоту матрицы ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите ширину матрицы ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j = 0; j < matr.GetLength(1); j++)
+       {
+        matr[i,j] = new Random().Next(1,10);
+       } 
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j = 0; j < matr.GetLength(1); j++)
+       {
+        Console.Write(matr[i,j] + " ");
+       } 
+    }
+}
+FillArray(array);
+PrintArray(array);
