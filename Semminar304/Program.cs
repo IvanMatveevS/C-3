50e20f21805dc3597482bf36a207a9c823fd29a1
Ivan <ivanmatveev1001@gmail.com>
2022-08-27 23:38:57 +0300

    сумма элиментов стоящих на диагонали в матрице

---
+++ Semminar304/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("Введите высоту матрицы ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите ширину матрицы ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j = 0; j < matr.GetLength(1); j++)
+       {
+        matr[i,j] = new Random().Next(1,10);
+       } 
+    }
+}
+int count = 0;
+void sqrt (int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j = 0; j < matr.GetLength(1); j++)
+       {
+        if( i == j ) count = count + matr[i,j];
+       } 
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j = 0; j < matr.GetLength(1); j++)
+       {
+        Console.Write(matr[i,j] + " ");
+       } 
+       Console.WriteLine();
+    }
+}
+FillArray(array);
+PrintArray(array);
+sqrt(array);
+Console.WriteLine("Сумма диагонали:");
+Console.WriteLine(count);
