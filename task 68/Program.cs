0f20d42e8b39a7de3730e8a9e5bdefa6682aa8bb
Ivan <ivanmatveev1001@gmail.com>
2022-09-04 20:49:41 +0300

    54

---
+++ task 68/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("введите первое число");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите второе число ");
+int n = int.Parse(Console.ReadLine());
+
+
+int RecursionNumbers(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m != 0 && n == 0)
+    {
+        return RecursionNumbers(m - 1, 1) ;  
+    }
+    else
+    {
+        return RecursionNumbers(m - 1, RecursionNumbers(m, n - 1));    
+    }
+}
+Console.WriteLine(RecursionNumbers (m , n));
