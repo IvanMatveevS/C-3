c75eb8904798a42f69ccf402235a42f08a5f7d33
Ivan <ivanmatveev1001@gmail.com>
2022-09-04 20:57:59 +0300

    62

---
+++ task 62/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+void PrintArray (int[,] array)
+{
+    {
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+}
+
+
+
+int n = 5;
+
+int[,] sqareMatrix = new int [n , n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+    sqareMatrix[i,j] = temp;
+    temp++;
+    if(i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+    j++;
+    else if(i < j  && i + j >= sqareMatrix.GetLength(0) - 1)
+    i++;
+    else if(i >= j  && i + j > sqareMatrix.GetLength(1) - 1)
+    j--;
+    else 
+    i--;
+}
+PrintArray(sqareMatrix);
+    
