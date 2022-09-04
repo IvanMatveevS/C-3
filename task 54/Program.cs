80175f60db70d2fd99cce7d113dca3d55bf042be
Ivan <ivanmatveev1001@gmail.com>
2022-09-04 21:04:57 +0300

    54

---
+++ task 54/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++);
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i,k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i,k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreatArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(100);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[ , ] array = new int[m , n];
+CreatArray(array);
+WriteArray(array);
+
+Console.WriteLine("перетос.");
+OrderArrayLines(array);
+WriteArray(array);
+
+
